a2161943e8b1f469c8899e78c199872366f92e11
ellenchart <echmeow@gmail.com>
2024-04-17 22:26:10 -0400

    Test
    Co-authored-by: jessicooper9 <jessicooper9@users.noreply.github.com>

--- Genre.h
+++ Genre.h
@@ -6,5 +6,73 @@
 /**
  * @brief Genre class
  *
- * This Genre class holds information about the genre of the movie.
+ * This Genre class holds information about the genre of
+ * the movie including its ID, genre.
  */
+
+class Genre
+{
+public:
+    /**
+     * @brief Construct a new Genre object
+     *
+     */
+    Genre() {}
+
+    Genre(crow::json::rvalue read);
+
+    /**
+     * @brief Get the Genre ID object
+     *
+     * @return int
+     */
+    int getGenreID() const;
+
+    /**
+     * @brief Get the Genre object
+     *
+     * @return std::string
+     */
+    std::string getGenre() const;
+
+    /**
+     * @brief Set the Genre ID object
+     *
+     * @param genreID
+     */
+    void setGenreID(int genreID);
+
+    /**
+     * @brief Set the Genre object
+     *
+     * @param genre
+     */
+    void setGenre(std::string genre);
+
+    /**
+     * @brief Convert the Genre object to JSON
+     *
+     * @return crow::json::wvalue
+     */
+    crow::json::wvalue convertToJson();
+
+    /**
+     * @brief Update the Genre object from JSON
+     *
+     * @param readValueJson
+     */
+    void updateFromJson(crow::json::rvalue readValueJson);
+
+private:
+    /**
+     * @brief Genre ID
+     */
+    std::string genreID;
+
+    /**
+     * @brief Genre
+     */
+    std::string genre;
+};
+
+#endif // GENRE_H
--- JellenReview.h
+++ JellenReview.h
@@ -1,8 +1,35 @@
-#ifndef BANK_ACCOUNT_H
-#define BANK_ACCOUNT_H
+// JellenReview.h
+/**
+ * @file JellenReview.h
+ * @brief Header file for the Jellen Review class.
+ *
+ * This file contains the declaration of the Jellen Review class along with its
+ * member functions and variables.
+ */
+#ifndef JELLEN_REVIEW_H
+#define JELLEN_REVIEW_H
 
 #include <string>
+#include "Movie.h"
 
-#include Movie.hclass BankAccount
+/**
+ * @class JellenReview
+ * @brief A class that offers jellen review activity.
+ *
+ * This class can perform actions such as set/get ratings, set/get
+ * reviews, and get/set if jellen would watch it again.
+ */
+class JellenReview
 {
-}
+public:
+    /**
+     * @brief Constructs a new BankAccount object.
+     * @param owner the name of the owner of the bank account.
+     * @param initialBalance the initial balance of the bank account.
+     */
+    JellenReview(std::string id, std::string review, int rating, bool wouldWatchAgain);
+
+private:
+};
+
+#endif // JELLEN_REVIEW_H
