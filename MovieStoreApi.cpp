f4542cf69962628cbe33bc8473fa7ab2b9530580
ellenchart <echmeow@gmail.com>
2024-04-24 23:53:34 -0400

    check

--- MovieStoreApi.cpp
+++ MovieStoreApi.cpp
@@ -16,55 +16,78 @@ using namespace std;
 using namespace crow;
 
 // Load resources from files.
-map<std::string, Actor> actorsMap = loadFromFile<Genre>("genres.json");
-map<std::string, Cart> cartsMap = loadFromFile<Artist>("artists.json");
-map<std::string, Director> directorsMap = loadFromFile<Record>("records.json");
-map<std::string, Genre> genresMap = loadFromFile<CompactDisc>("compactdiscs.json");
-map<std::string, JellenReview> reviewsMap = loadFromFile<Genre>("genres.json");
-map<std::string, Movie> moviMap = loadFromFile<Artist>("artists.json");
-map<std::string, Personnel> recordsMap = loadFromFile<Record>("records.json");
+map<std::string, Actor> actorsMap = loadFromFile<Actor>("actors.json");
+map<std::string, Cart> cartsMap = loadFromFile<Cart>("carts.json");
+map<std::string, Director> directorsMap = loadFromFile<Director>("directors.json");
+map<std::string, Genre> genresMap = loadFromFile<Genre>("genres.json");
+map<std::string, JellenReview> reviewsMap = loadFromFile<JellenReview>("reviews.json");
+map<std::string, Movie> moviesMap = loadFromFile<Movie>("movies.json");
+map<std::string, Personnel> personnelMap = loadFromFile<Personnel>("personnel.json");
 
 int main() 
 {
     // Create GenericAPIs to use in the CROW_ROUTES
+    GenericAPI<Actor>::resourceMap = actorsMap;
+    GenericAPI<Cart>::resourceMap = carts;
+    GenericAPI<Director>::resourceMap = directorsMap;
     GenericAPI<Genre>::resourceMap = genresMap;
-    GenericAPI<Artist>::resourceMap = artistsMap;
-    GenericAPI<Record>::resourceMap = recordsMap;
-    GenericAPI<CompactDisc>::resourceMap = compactDiscsMap;
+    GenericAPI<JellenReview>::resourceMap = reviewsMap;
+    GenericAPI<Movie>::resourceMap = moviesMap;
+    GenericAPI<Personnel>::resourceMap = personnelMap;
 
     // Setup the simple web service.
     SimpleApp app;
 
     // Define resources:
 
-    // Genres
+    // Actor
+    CROW_ROUTE(app, "/api/actors").methods("POST"_method)(GenericAPI<Actor>::createResource);
+    CROW_ROUTE(app, "/api/actors").methods("GET"_method)(GenericAPI<Actor>::readAllResources);
+    CROW_ROUTE(app, "/api/actors/<string>").methods("GET"_method)(GenericAPI<Actor>::readResource);
+    CROW_ROUTE(app, "/api/actors/<string>").methods("PUT"_method)(GenericAPI<Actor>::updateResource);
+    CROW_ROUTE(app, "/api/actors/<string>").methods("DELETE"_method)(GenericAPI<Actor>::deleteResource);
+
+    // Cart
+    CROW_ROUTE(app, "/api/carts").methods("POST"_method)(GenericAPI<Cart>::createResource);
+    CROW_ROUTE(app, "/api/carts").methods("GET"_method)(GenericAPI<Cart>::readAllResources);
+    CROW_ROUTE(app, "/api/carts/<string>").methods("GET"_method)(GenericAPI<Cart>::readResource);
+    CROW_ROUTE(app, "/api/carts/<string>").methods("PUT"_method)(GenericAPI<Cart>::updateResource);
+    CROW_ROUTE(app, "/api/carts/<string>").methods("DELETE"_method)(GenericAPI<Cart>::deleteResource);
+
+    // Director
+    CROW_ROUTE(app, "/api/directors").methods("POST"_method)(GenericAPI<Director>::createResource);
+    CROW_ROUTE(app, "/api/directors").methods("GET"_method)(GenericAPI<Director>::readAllResources);
+    CROW_ROUTE(app, "/api/directors/<string>").methods("GET"_method)(GenericAPI<Director>::readResource);
+    CROW_ROUTE(app, "/api/directors/<string>").methods("PUT"_method)(GenericAPI<Director>::updateResource);
+    CROW_ROUTE(app, "/api/directors/<string>").methods("DELETE"_method)(GenericAPI<Director>::deleteResource);
+
+    // Genre
     CROW_ROUTE(app, "/api/genres").methods("POST"_method)(GenericAPI<Genre>::createResource);
     CROW_ROUTE(app, "/api/genres").methods("GET"_method)(GenericAPI<Genre>::readAllResources);
     CROW_ROUTE(app, "/api/genres/<string>").methods("GET"_method)(GenericAPI<Genre>::readResource);
     CROW_ROUTE(app, "/api/genres/<string>").methods("PUT"_method)(GenericAPI<Genre>::updateResource);
     CROW_ROUTE(app, "/api/genres/<string>").methods("DELETE"_method)(GenericAPI<Genre>::deleteResource);
 
-    // Artist
-    CROW_ROUTE(app, "/api/artists").methods("POST"_method)(GenericAPI<Artist>::createResource);
-    CROW_ROUTE(app, "/api/artists").methods("GET"_method)(GenericAPI<Artist>::readAllResources);
-    CROW_ROUTE(app, "/api/artists/<string>").methods("GET"_method)(GenericAPI<Artist>::readResource);
-    CROW_ROUTE(app, "/api/artists/<string>").methods("PUT"_method)(GenericAPI<Artist>::updateResource);
-    CROW_ROUTE(app, "/api/artists/<string>").methods("DELETE"_method)(GenericAPI<Artist>::deleteResource);
+    // JellenReview
+    CROW_ROUTE(app, "/api/reviews").methods("POST"_method)(GenericAPI<JellenReview>::createResource);
+    CROW_ROUTE(app, "/api/reviews").methods("GET"_method)(GenericAPI<JellenReview>::readAllResources);
+    CROW_ROUTE(app, "/api/reviews/<string>").methods("GET"_method)(GenericAPI<JellenReview>::readResource);
+    CROW_ROUTE(app, "/api/reviews/<string>").methods("PUT"_method)(GenericAPI<JellenReview>::updateResource);
+    CROW_ROUTE(app, "/api/reviews/<string>").methods("DELETE"_method)(GenericAPI<JellenReview>::deleteResource);
+    
+    // Movie
+    CROW_ROUTE(app, "/api/movies").methods("POST"_method)(GenericAPI<Movie>::createResource);
+    CROW_ROUTE(app, "/api/movies").methods("GET"_method)(GenericAPI<Movie>::readAllResources);
+    CROW_ROUTE(app, "/api/movies/<string>").methods("GET"_method)(GenericAPI<Movie>::readResource);
+    CROW_ROUTE(app, "/api/movies/<string>").methods("PUT"_method)(GenericAPI<Movie>::updateResource);
+    CROW_ROUTE(app, "/api/movies/<string>").methods("DELETE"_method)(GenericAPI<Movie>::deleteResource);
 
-    // Records
-    CROW_ROUTE(app, "/api/records").methods("POST"_method)(GenericAPI<Record>::createResource);
-    CROW_ROUTE(app, "/api/records").methods("GET"_method)(GenericAPI<Record>::readAllResources);
-    CROW_ROUTE(app, "/api/records/<string>").methods("GET"_method)(GenericAPI<Record>::readResource);
-    CROW_ROUTE(app, "/api/records/<string>").methods("PUT"_method)(GenericAPI<Record>::updateResource);
-    CROW_ROUTE(app, "/api/records/<string>").methods("DELETE"_method)(GenericAPI<Record>::deleteResource);
-
-    // CompactDiscs
-    CROW_ROUTE(app, "/api/compactdiscs").methods("POST"_method)(GenericAPI<CompactDisc>::createResource);
-    CROW_ROUTE(app, "/api/compactdiscs").methods("GET"_method)(GenericAPI<CompactDisc>::readAllResources);
-    CROW_ROUTE(app, "/api/compactdiscs/<string>").methods("GET"_method)(GenericAPI<CompactDisc>::readResource);
-    CROW_ROUTE(app, "/api/compactdiscs/<string>").methods("PUT"_method)(GenericAPI<CompactDisc>::updateResource);
-    CROW_ROUTE(app, "/api/compactdiscs/<string>").methods("DELETE"_method)(GenericAPI<CompactDisc>::deleteResource);
-
+    // Personnel
+    CROW_ROUTE(app, "/api/personnel").methods("POST"_method)(GenericAPI<Personnel>::createResource);
+    CROW_ROUTE(app, "/api/personnel").methods("GET"_method)(GenericAPI<Personnel>::readAllResources);
+    CROW_ROUTE(app, "/api/personnel/<string>").methods("GET"_method)(GenericAPI<Personnel>::readResource);
+    CROW_ROUTE(app, "/api/personnel/<string>").methods("PUT"_method)(GenericAPI<Personnel>::updateResource);
+    CROW_ROUTE(app, "/api/personnel/<string>").methods("DELETE"_method)(GenericAPI<Personnel>::deleteResource);
 
     // Run the web service app.
     app.port(18080).run();
@@ -72,8 +95,11 @@ int main()
     // After the web service stops running:
 
     // Perform persistence of resources
+    saveToFile<Actor>(GenericAPI<Actor>::resourceMap, "actors.json");
+    saveToFile<Cart>(GenericAPI<Cart>::resourceMap, "carts.json");
+    saveToFile<Director>(GenericAPI<Director>::resourceMap, "directors.json");
     saveToFile<Genre>(GenericAPI<Genre>::resourceMap, "genres.json");
-    saveToFile<Artist>(GenericAPI<Artist>::resourceMap, "artists.json");
-    saveToFile<Record>(GenericAPI<Record>::resourceMap, "records.json");
-    saveToFile<CompactDisc>(GenericAPI<CompactDisc>::resourceMap, "compactdiscs.json");
+    saveToFile<JellenReview>(GenericAPI<JellenReview>::resourceMap, "reviews.json");
+    saveToFile<Movie>(GenericAPI<Movie>::resourceMap, "movies.json");
+    saveToFile<Personnel>(GenericAPI<Personnel>::resourceMap, "personnel.json");
 }
