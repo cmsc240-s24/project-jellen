cc9658906032427ece7870d982b34baa74a14c68
jessicooper9 <jessi.cooper@richmond.edu>
2024-04-25 16:39:01 -0400

    updated movie test

--- MovieTest.cpp
+++ MovieTest.cpp
@@ -55,18 +55,20 @@ TEST_CASE("Testing Movie Class")
         "https://www.imdb.com/title/tt0108052/mediaviewer/rm1610023168/?ref_=tt_ov_i","personnel":[{"id": "1"}],"genre":{"id":"1"}, "review":{"id":"1"}})"));
 }
         // Create the update json.
-        json::rvalue updateJson = json::load(R"({"name":"Adam Sandler","id":"2","canImprov":true})");
+        json::rvalue updateJson = json::load(R"({"id":"1","title":"Fight Club","cost":3.99,"length": 139, "linkToMoviePoster":
+        "https://www.imdb.com/title/tt0137523/mediaviewer/rm1412004864/?ref_=tt_ov_i","personnel":[{"id": "1"}],"genre":{"id":"1"}, "review":{"id":"1"}})");
 
         // Update the Movie with the updateFromJson method. 
         testMovie.updateFromJson(updateJson);
 
         // Check the updated values.
         CHECK(testMovie.getID() == "1");
-        CHECK(testMovie.getTitle() == "Schindler's List");
+        CHECK(testMovie.getTitle() == "Fight Club");
         CHECK(testMovie.getCost() == 3.99);
-        CHECK(testMovie.getLength() == 195);
+        CHECK(testMovie.getLength() == 139);
         CHECK(testMovie.getGenre().getID() == "1");
         CHECK(testMovie.getPersonnel()[0].getID() == "1");
-        CHECK(testMovie.getPoster() == "https://www.imdb.com/title/tt0108052/mediaviewer/rm1610023168/?ref_=tt_ov_i");
+        CHECK(testMovie.getReview().getID() == "1");
+        CHECK(testMovie.getPoster() == "https://www.imdb.com/title/tt0137523/mediaviewer/rm1412004864/?ref_=tt_ov_i");
     }
 }
