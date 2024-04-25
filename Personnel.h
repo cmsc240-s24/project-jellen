9db0ec4d979d5e00891faead4890e66fa46674e9
jessicooper9 <jessi.cooper@richmond.edu>
2024-04-24 21:23:20 -0400

    oop

--- Personnel.h
+++ Personnel.h
@@ -6,8 +6,7 @@
  * This file contains the declaration of the Personnel class along with its
  * member functions and variables.
  */
-
-#include PERSONNEL_H
+#ifndef PERSONNEL_H
 #define PERSONNEL_H
 
 #include <crow.h>
@@ -62,7 +61,7 @@ class Personnel
          * @brief Convert the Personnel object to JSON
          * @return crow::json::wvalue
          */
-        updateFromJson(crow::json::rvalue readValueJson);
+        void updateFromJson(crow::json::rvalue readValueJson);
         
         /**
          * @brief Convert the Cart object to JSON
