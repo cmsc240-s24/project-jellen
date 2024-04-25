88f918b58a5be37be23ec0f7c6e51665d33fd227
jessicooper9 <jessi.cooper@richmond.edu>
2024-04-25 14:18:06 -0400

    update

--- JellenReviewTest.cpp
+++ JellenReviewTest.cpp
@@ -19,8 +19,8 @@ with its catchy songs and uplifting storyline. With its infectious energy and re
 
         // Check that the constructor properly loaded the values.
         CHECK(testJellenReview.getID() == "1");
-        CHECK(testJellenReview.getReview() == "'High School Musical' is a feel-good teen musical that charms audiences 
-with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.");
+        CHECK(testJellenReview.getReview() == "'High School Musical' is a feel-good teen musical that charms audiences" 
+        "with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.");
         CHECK(testJellenReview.getRating() == 7);
         CHECK(testJellenReview.getWouldWatchAgain() == true);
     }
@@ -29,9 +29,9 @@ with its catchy songs and uplifting storyline. With its infectious energy and re
     SUBCASE("Testing the convertToJson Method") 
     {
         // Create a new JellenReview class from json.
-        JellenReview testJellenReview(json::load(R"({"id":"1","review":"'High School Musical' is a feel-good teen musical that charms audiences 
-with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.","rating":7,
-"would watch again":true})"));
+        JellenReview testJellenReview(json::load(R"({"id":"1","review":"'High School Musical' is a feel-good teen musical that charms audiences " 
+        "with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.","rating":7,
+        "would watch again":true})"));
 
         // Convert the JellenReview class to json using the convertToJson method.
         json::wvalue jsonOutput = testJellenReview.convertToJson();
@@ -41,8 +41,8 @@ with its catchy songs and uplifting storyline. With its infectious energy and re
 
         // Check the values.
         CHECK(jsonReadValue["id"].s() == "1");
-        CHECK(jsonReadValue["review"].s() == "'High School Musical' is a feel-good teen musical that charms audiences 
-with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.");
+        CHECK(jsonReadValue["review"].s() == "'High School Musical' is a feel-good teen musical that charms audiences "
+        "with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.");
         CHECK(jsonReadValue["rating"].i() == 7);
         CHECK(jsonReadValue["would watch again"].b() == true);
     }
@@ -51,13 +51,13 @@ with its catchy songs and uplifting storyline. With its infectious energy and re
     SUBCASE("Testing updateFromJson Method") 
     {
         // Create a new JellenReview class from json.
-        JellenReview testJellenReview(json::load(R"({"id":"1","review":"'High School Musical' is a feel-good teen musical that charms audiences 
-with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.","rating":7,
-"would watch again":true})"));
+        JellenReview testJellenReview(json::load(R"({"id":"1","review":"'High School Musical' is a feel-good teen musical that charms audiences "
+        "with its catchy songs and uplifting storyline. With its infectious energy and relatable characters, it's a fun ride for fans of musicals and high school dramas alike.","rating":7,
+        "would watch again":true})"));
 
         // Create the update json.
-        json::rvalue updateJson = json::load(R"({"id":"1","review":"'Good Will Hunting' is a poignant drama that explores themes of intellect, identity, 
-        and friendship with powerful performances from its cast. Matt Damon and Robin Williams shine in this emotionally resonant tale of self-discovery and second chances.",
+        json::rvalue updateJson = json::load(R"({"id":"1","review":"'Good Will Hunting' is a poignant drama that explores themes of intellect, identity, " 
+        "and friendship with powerful performances from its cast. Matt Damon and Robin Williams shine in this emotionally resonant tale of self-discovery and second chances.",
         "rating":9,"would watch again":true})");
 
         // Update the Director with the updateFromJson method. 
@@ -65,8 +65,8 @@ with its catchy songs and uplifting storyline. With its infectious energy and re
 
         // Check the updated values.
         CHECK(testJellenReview.getID() == "1");
-        CHECK(testJellenReview.getReview() == "'Good Will Hunting' is a poignant drama that explores themes of intellect, identity, 
-        and friendship with powerful performances from its cast. Matt Damon and Robin Williams shine in this emotionally resonant tale of self-discovery and second chances.");
+        CHECK(testJellenReview.getReview() == "'Good Will Hunting' is a poignant drama that explores themes of intellect, identity, " 
+        "and friendship with powerful performances from its cast. Matt Damon and Robin Williams shine in this emotionally resonant tale of self-discovery and second chances.");
         CHECK(testJellenReview.getRating() == 9);
         CHECK(testJellenReview.getWouldWatchAgain() == true);
     }
