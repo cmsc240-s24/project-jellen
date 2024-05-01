5328d5c11215ce0be9bbbd94302528fd56c396bb
jessicooper9 <jessi.cooper@richmond.edu>
2024-04-30 23:17:58 -0400

    commit

--- MovieStoreAPI.cpp
+++ MovieStoreAPI.cpp
@@ -50,48 +50,38 @@ int main()
     cout << "after crow actor route" << endl;
 
     // Director
-    cout << "before Director actor route" << endl;
     CROW_ROUTE(app, "/api/directors").methods("POST"_method)(GenericAPI<Director>::createResource);
     CROW_ROUTE(app, "/api/directors").methods("GET"_method)(GenericAPI<Director>::readAllResources);
     CROW_ROUTE(app, "/api/directors/<string>").methods("GET"_method)(GenericAPI<Director>::readResource);
     CROW_ROUTE(app, "/api/directors/<string>").methods("PUT"_method)(GenericAPI<Director>::updateResource);
     CROW_ROUTE(app, "/api/directors/<string>").methods("DELETE"_method)(GenericAPI<Director>::deleteResource);
-    cout << "after Director  route" << endl;
     
     // Genre
-    cout << "before Genre actor route" << endl;
     CROW_ROUTE(app, "/api/genres").methods("POST"_method)(GenericAPI<Genre>::createResource);
     CROW_ROUTE(app, "/api/genres").methods("GET"_method)(GenericAPI<Genre>::readAllResources);
     CROW_ROUTE(app, "/api/genres/<string>").methods("GET"_method)(GenericAPI<Genre>::readResource);
     CROW_ROUTE(app, "/api/genres/<string>").methods("PUT"_method)(GenericAPI<Genre>::updateResource);
     CROW_ROUTE(app, "/api/genres/<string>").methods("DELETE"_method)(GenericAPI<Genre>::deleteResource);
-    cout << "after genre  route" << endl;
 
     // JellenReview
-    cout << "before jellenReview api methods:" << endl;
     CROW_ROUTE(app, "/api/reviews").methods("POST"_method)(GenericAPI<JellenReview>::createResource);
     CROW_ROUTE(app, "/api/reviews").methods("GET"_method)(GenericAPI<JellenReview>::readAllResources);
     CROW_ROUTE(app, "/api/reviews/<string>").methods("GET"_method)(GenericAPI<JellenReview>::readResource);
     CROW_ROUTE(app, "/api/reviews/<string>").methods("PUT"_method)(GenericAPI<JellenReview>::updateResource);
     CROW_ROUTE(app, "/api/reviews/<string>").methods("DELETE"_method)(GenericAPI<JellenReview>::deleteResource);
-    cout << "after jellenReview api methods:" << endl;
     // Movie
-    cout << "before movie api methods:" << endl;
     CROW_ROUTE(app, "/api/movies").methods("POST"_method)(GenericAPI<Movie>::createResource);
     CROW_ROUTE(app, "/api/movies").methods("GET"_method)(GenericAPI<Movie>::readAllResources);
     CROW_ROUTE(app, "/api/movies/<string>").methods("GET"_method)(GenericAPI<Movie>::readResource);
     CROW_ROUTE(app, "/api/movies/<string>").methods("PUT"_method)(GenericAPI<Movie>::updateResource);
     CROW_ROUTE(app, "/api/movies/<string>").methods("DELETE"_method)(GenericAPI<Movie>::deleteResource);
-    cout << "after movie  route" << endl;
 
     // Personnel
-    cout << "before personnel api methods:" << endl;
     CROW_ROUTE(app, "/api/personnel").methods("POST"_method)(GenericAPI<Personnel>::createResource);
     CROW_ROUTE(app, "/api/personnel").methods("GET"_method)(GenericAPI<Personnel>::readAllResources);
     CROW_ROUTE(app, "/api/personnel/<string>").methods("GET"_method)(GenericAPI<Personnel>::readResource);
     CROW_ROUTE(app, "/api/personnel/<string>").methods("PUT"_method)(GenericAPI<Personnel>::updateResource);
     CROW_ROUTE(app, "/api/personnel/<string>").methods("DELETE"_method)(GenericAPI<Personnel>::deleteResource);
-    cout << "after personnel api methods:" << endl;
     
     // Run the web service app.
     app.port(17892).run();
@@ -99,7 +89,6 @@ int main()
     // After the web service stops running:
 
     // Perform persistence of resources
-    cout << "persistence: " << endl;
     saveToFile<Actor>(GenericAPI<Actor>::resourceMap, "actors.json");
     saveToFile<Director>(GenericAPI<Director>::resourceMap, "directors.json");
     saveToFile<Genre>(GenericAPI<Genre>::resourceMap, "genres.json");
